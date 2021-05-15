9c965af391c542e13f5f0feab78536412f9d7774
ShreyasDobhal <shreyasdobhal@gmail.com>
2021-05-15 14:20:12 +0530

    Added input form elements

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,15 +2,32 @@ import React, { Component } from 'react';
 // import ReactDOM from 'react-dom';
 import SignatureCanvas from 'react-signature-canvas';
 import CanvasOptions from './CanvasOptions';
+import Input from './Input';
+import _ from 'lodash';
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
+            error: {
+                'email': null,
+                'name': null,
+                'upi': null,
+                'eid': null,
+                'hq': null,
+                'mobile': null,
+            }
         };
 
+        this.inputRef = {
+            'email': React.createRef(),
+            'name': React.createRef(),
+            'upi': React.createRef(),
+            'eid': React.createRef(),
+            'hq': React.createRef(),
+            'mobile': React.createRef()
+        }
         this.signCanvasInstance = React.createRef();
     }
 
@@ -27,13 +44,74 @@ class HomePage extends Component {
         this.signCanvasInstance.current.clear();
     }
 
+    validateInput = (element) => {
+        const field = _.find(_.keys(this.inputRef), (field) => this.inputRef[field] === element);
+        const inputElement = element.current;
+        if (_.isEmpty(inputElement.value)) {
+            this.setState({
+                error: {
+                    ...this.state.error,
+                    [field]: 'This field cannot be empty'
+                }
+            });
+        } else if (!_.isNull(this.state.error[field])) {
+            this.setState({
+                error: {
+                    ...this.state.error,
+                    [field]: null
+                }
+            });
+        }
+        console.log(field, element);
+    }
+
     render() {
         return (
             <div className='App'>
                 <div className='letter-container'>
-                    <div>
-                        <h1><img className='letter-icon' src='icon.png' />Abbott</h1>
-                        <p>Hey</p>
+                    <div className='letter-header'>
+                        <h1><img className='letter-icon' src='icon.png' alt='Abbott logo' />Abbott</h1>
+                        <p>All India Abbott Employees Union</p>
+                    </div>
+                    <div className='letter-body'>
+                        <p><b>Subject: </b>Comred Amit Mehta continue as a President.</p>
+                        <p>
+                            I confirm my approval that Com. Amit Mehta continue as a president
+                            of Abbott all India Employee's union and to give his honorary
+                            service even after his retirement.
+                        </p>
+                    </div>
+                    <div className='letter-form'>
+                        <Input 
+                            ref={this.inputRef['email']}
+                            label='Email'
+                            error={this.state.error['email']}
+                            onBlur={this.validateInput} />
+                        <Input 
+                            ref={this.inputRef['name']} 
+                            label='Name'
+                            error={this.state.error['name']}
+                            onBlur={this.validateInput} />
+                        <Input 
+                            ref={this.inputRef['upi']} 
+                            label='UPI ID' 
+                            error={this.state.error['upi']}
+                            onBlur={this.validateInput} />
+                        <Input 
+                            ref={this.inputRef['eid']} 
+                            label='Employee Id' 
+                            error={this.state.error['eid']}
+                            onBlur={this.validateInput} />
+                        <Input 
+                            ref={this.inputRef['hq']} 
+                            label='Head Quarter' 
+                            error={this.state.error['hq']}
+                            onBlur={this.validateInput} />
+                        <Input 
+                            ref={this.inputRef['mobile']} 
+                            label='Mobile Number' 
+                            error={this.state.error['mobile']}
+                            onBlur={this.validateInput} />
                     </div>
                     <div className='sign-container'>
                         <h3>Signature:</h3>
---
+++ src/components/Input.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import _ from 'lodash';
+
+class Input extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isFocused: false
+        }
+    }
+
+    render() {
+        return (
+            <div className={`letter-input${
+                    this.state.isFocused
+                        ? ' input-focused'
+                        : !_.isNull(this.props.error) ? ' input-error' : '' 
+                 }`}
+                 onFocus={() => {
+                    this.setState({isFocused: true});
+                    if (this.props.onFocus) {
+                        this.props.onFocus(this.props.forwardedRef);
+                    }
+                 }}
+                 onBlur={() => {
+                    this.setState({isFocused: false});
+                    if (this.props.onBlur) {
+                        this.props.onBlur(this.props.forwardedRef);
+                    }
+                 }}>
+                <h3>{this.props.label}: <sup> *</sup></h3>
+                <input type='text' ref={this.props.forwardedRef} />
+                {
+                    !_.isNull(this.props.error) ? (
+                        <p className='error-message'>{this.props.error}</p>
+                    ) : null
+                }
+            </div>
+        )
+    }
+}
+
+export default React.forwardRef((props, ref) => {
+    return <Input {...props} forwardedRef={ref} />
+});
